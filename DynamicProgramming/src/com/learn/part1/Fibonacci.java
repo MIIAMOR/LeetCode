9bfa62d567cb1b35410decd2b27a5676e56615fd
计算机1805-于斌-20184573 <67416178+MIIAMOR@users.noreply.github.com>
2021-05-10 12:25:46 +0800

    动态规划题目*3

---
+++ DynamicProgramming/src/com/learn/part1/Fibonacci.java
@@ -0,0 +1,72 @@
+package com.learn.part1;
+
+/**
+ * 动态规划题目：斐波那契数列
+ */
+public class Fibonacci {
+    /**
+     * 递归算法，重复调用自身。可能栈内存溢出
+     *
+     * @param n
+     * @return
+     */
+    public int fib1(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fib1(n - 1) + fib1(n - 2);
+        }
+    }
+
+    /**
+     * 动态规划算法，可以优化，不必开辟数组
+     *
+     * @param n
+     * @return
+     */
+    public int fib2(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            for (int i = 2; i < dp.length; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+
+    /**
+     * 动态规划改良，减少内存消耗
+     * @param n
+     * @return
+     */
+    public int fib3(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            int sum = 0, pre = 0, curr = 1;
+            for (int i = 1; i < n; i++) {
+                sum = curr + pre;
+                pre = curr;
+                curr = sum;
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        Fibonacci f = new Fibonacci();
+        System.out.println(f.fib2(5));
+        System.out.println(f.fib1(5));
+        System.out.println(f.fib3(5));
+    }
+}
---
+++ leetcode-master/README.md
@@ -0,0 +1,21 @@
+# 介绍
+
+题解预览地址：[https://leetcode.wang](https://leetcode.wang)，推荐电脑端打开，手机打开的话将页面滑到最上边，左上角是菜单
+
+leetcode 题目地址 https://leetcode.com/problemset/all/
+
+github 项目地址：https://github.com/wind-liang/leetcode 
+
+为什么刷题：https://leetcode.wang/leetcode100%E6%96%A9%E5%9B%9E%E9%A1%BE.html
+
+知乎开设了专栏，同步更新：[https://zhuanlan.zhihu.com/leetcode1024](https://zhuanlan.zhihu.com/leetcode1024)，关注后可以及时收到更新，网站有时候可能出问题打不开，建议关注一下知乎专栏备用
+
+准备刷一道，总结一道。
+
+可以加好友一起交流。
+
+微信： 17771420231
+
+公众号： windliang，更新编程相关
+
+如果觉得对你有帮助，记得给一个 star 哦 ^ ^
